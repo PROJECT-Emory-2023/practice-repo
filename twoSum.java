d66f73cabf1d213f38a18991fe4257603d1147f1
AChung1020 <achun46@emory.edu>
2023-09-21 18:56:33 -0400

    fixed

--- twoSum.java
+++ twoSum.java
@@ -1,21 +1,25 @@
+import java.util.HashMap;
+
 public class twoSum {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-        int arr[] = {2,7,11,15};
-        twoSum(arr, 9);
+        int arr1[] = {2, 7, 11, 15};
+        int[] result1 = twoSum(arr1, 9);
+        System.out.println("Result 1: " + result1[0] + ", " + result1[1]);
 
-        int arr[] = {3,2,4};
-        twoSum(arr, 6);
+        int arr2[] = {3, 2, 4};
+        int[] result2 = twoSum(arr2, 6);
+        System.out.println("Result 2: " + result2[0] + ", " + result2[1]);
 
-        int arr[] = {3,3};
-        twoSum(arr, 6);
+        int arr3[] = {3, 3};
+        int[] result3 = twoSum(arr3, 6);
+        System.out.println("Result 3: " + result3[0] + ", " + result3[1]);
     }
 
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         //code here
 
         return 0;
     }
-    
 }
