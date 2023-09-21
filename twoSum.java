d33c9271526e64709a6036a65bab63cc5ac48dad
AChung1020 <achun46@emory.edu>
2023-09-21 18:53:27 -0400

    add

--- twoSum.java
+++ twoSum.java
@@ -2,6 +2,14 @@ public class twoSum {
 
     public static void main (String[] args) {
 
+        int arr[] = {2,7,11,15};
+        twoSum(arr, 9);
+
+        int arr[] = {3,2,4};
+        twoSum(arr, 6);
+
+        int arr[] = {3,3};
+        twoSum(arr, 6);
     }
 
     public int[] twoSum(int[] nums, int target) {
