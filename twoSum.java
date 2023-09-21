4458dc1c22be2e42b1037d5cc12127217f0226bd
AChung1020 <achun46@emory.edu>
2023-09-21 18:51:21 -0400

    two sum

---
+++ twoSum.java
@@ -0,0 +1,13 @@
+public class twoSum {
+
+    public static void main (String[] args) {
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        //code here
+
+        return 0;
+    }
+    
+}
