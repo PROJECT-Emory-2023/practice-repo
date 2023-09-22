0ce49d84e45edec76b4102334ead1cfd7ed0d8f8
jiyaushah <145076557+jiyaushah@users.noreply.github.com>
2023-09-21 20:30:53 -0400

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -20,7 +20,7 @@ public class twoSum {
     public static int[] twoSum(int[] nums, int target) {
         int[] array = new int[2];
         for (int i = 0; i < nums.length; i++){
-            for (int j - i+1; i< nums.length; j++){
+            for (int j = i+1; i< nums.length; j++){
                 if (nums[i] + nums[j] == target){
                     array = {i, j};
                     break;
