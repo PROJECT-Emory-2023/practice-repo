7f917f304a5ff2088a862cb94180eeef39a51c51
Cyebukayire <peacecyebukayire@gmail.com>
2023-09-21 20:03:15 -0400

    fix return value

--- twoSum.java
+++ twoSum.java
@@ -19,7 +19,9 @@ public class twoSum {
 
     public static int[] twoSum(int[] nums, int target) {
         //code here
-
-        return 0;
+        for(int i=0; i<nums.length; i++){
+            nums[i] = nums[i] + target;
+        }
+        return nums;
     }
 }
