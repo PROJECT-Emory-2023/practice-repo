75d2d1bbf1aa2fa7bb48b923e9664920144cdbd7
GABYBONWITT <gabriellabonwitt@gmail.com>
2023-09-21 20:19:24 -0400

    I completed twoSum

--- twoSum.java
+++ twoSum.java
@@ -18,8 +18,17 @@ public class twoSum {
     }
 
     public static int[] twoSum(int[] nums, int target) {
-        //code here
+        int[] result = new int[2];
 
-        return 0;
+        for (int i =0; i<nums.length; i++){
+            for (int j =0; j<nums.length; j++){
+                if (nums[i]+ nums[j] == target){ 
+                    result[0] = i; 
+                    result[1] = j; 
+                }
+            }
+        }
+
+        return result;
     }
 }
